38264747d5e1abba42ef6037bb5414747b4a211f
udaycoder <udayanbaidya@gmail.com>
2017-11-09 15:59:20 +0530

    Added working code for machine learning module

--- machine_learning.py
+++ machine_learning.py
@@ -2,8 +2,23 @@ from sklearn.tree import DecisionTreeRegressor
 import numpy as np
 import pandas
 
+tourist=pandas.read_csv("csvfile.csv")
+
+mapping = {'Yes': 1, 'No': 0 }
+
+columns = tourist.columns.tolist()
+
+target="Rating"
+columns=[c for c in columns if c not in["Rating","City"]]
+
+for column in columns:
+    tourist=tourist.replace({column: mapping})
+
+train= tourist.sample(frac=0.8,random_state=1)
+test=  tourist.loc[~tourist.index.isin(train.index)]
+
 regr = DecisionTreeRegressor(max_depth=2)
 
-regr.fit(train_attributes,train_target)
+regr.fit(train[columns],train[target])
 
-print(regr.score(test_attributes,test_target))
+print(regr.score(test[columns],test[target]))
