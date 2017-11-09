949650230b0621e4188fbcf93877dda2ff46676b
udaycoder <udayanbaidya@gmail.com>
2017-11-09 21:27:47 +0530

    Added code for feature selection in ml.py

--- machine_learning.py
+++ machine_learning.py
@@ -1,6 +1,7 @@
 from sklearn.tree import DecisionTreeRegressor
 import numpy as np
 import pandas
+from sklearn.feature_selection import SelectKBest,mutual_info_regression
 
 tourist=pandas.read_csv("csvfile.csv")
 
@@ -13,12 +14,24 @@ columns=[c for c in columns if c not in["Rating","City"]]
 
 for column in columns:
     tourist=tourist.replace({column: mapping})
+    
 
 train= tourist.sample(frac=0.8,random_state=1)
 test=  tourist.loc[~tourist.index.isin(train.index)]
 
+trainAttributes=train[columns].astype('float64')
+trainTarget=train[target].astype('float64')
+testAttributes=test[columns].astype('float64')
+testTarget=test[target].astype('float64')
+
+selector = SelectKBest(mutual_info_regression,k=8).fit(trainAttributes,trainTarget)
+
+newtrainX= selector.transform(trainAttributes)
+
+newtestX=  selector.transform(testAttributes)
+
 regr = DecisionTreeRegressor(max_depth=2)
 
-regr.fit(train[columns],train[target])
+regr.fit(newtrainX,trainTarget)
 
-print(regr.score(test[columns],test[target]))
+print(regr.score(newtestX,testTarget))
