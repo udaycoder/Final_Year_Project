adc80a4efaf0e625ec4d729dc76869acccdc3f49
udaycoder <udayanbaidya@gmail.com>
2018-03-21 12:01:54 +0530

    Created pickling architecture for web app

---
+++ create_model.py
@@ -0,0 +1,60 @@
+from sklearn.tree import DecisionTreeRegressor
+import numpy as np
+import pandas
+from sklearn.feature_selection import SelectKBest,mutual_info_regression
+from sklearn import linear_model
+import pickle
+from sklearn.metrics import f1_score,precision_score,recall_score,accuracy_score,mean_squared_error,confusion_matrix
+
+tourist=pandas.read_csv("csvfile.csv")
+
+mapping = {'Yes': 1, 'No': 0 }
+
+columns = tourist.columns.tolist()
+
+target="Rating"
+columns=[c for c in columns if c not in["Rating","City"]]
+
+for column in columns:
+    tourist=tourist.replace({column: mapping})
+    
+
+train= tourist.sample(frac=0.8,random_state=1)
+test=  tourist.loc[~tourist.index.isin(train.index)]
+
+trainAttributes=train[columns].astype('float64')
+trainTarget=train[target].astype('float64')
+testAttributes=test[columns].astype('float64')
+testTarget=test[target].astype('float64')
+testTarget=list(testTarget)
+
+regr = DecisionTreeRegressor(max_depth=2)
+#regr=linear_model.LinearRegression()
+
+regr.fit(trainAttributes,trainTarget)
+
+filename = 'MachineLearningModel.pkl'
+pickle.dump(regr, open(filename, 'wb'))
+
+count=0;
+
+predictedList=[]
+actualList=[]
+
+for x in range(0,len(testTarget)):
+    h=testAttributes.iloc[x]
+    h=np.array(h).reshape(1,-1)
+    p= int(round(float(regr.predict(h))))
+    a= int(round(float(testTarget[x])))
+    predictedList.append(p)
+    actualList.append(a)
+    #print(regr.predict(h)," ",testTarget[x])
+
+print("Accuracy Score: ",accuracy_score(actualList,predictedList))
+print("Mean Squared Error: ",mean_squared_error(actualList,predictedList))
+print("F1_score: ",f1_score(actualList,predictedList,average="macro"))
+print("Precision Score: ",precision_score(actualList,predictedList,average="macro"))
+print("Recall Score: ",recall_score(actualList,predictedList,average="macro"))
+print("Confusion Matrix: ")
+print(confusion_matrix(actualList,predictedList))
+#print(regr.score(newtestX,testTarget))
--- machine_learning.py
+++ machine_learning.py
@@ -1,57 +1,12 @@
-from sklearn.tree import DecisionTreeRegressor
+import pickle
 import numpy as np
-import pandas
-from sklearn.feature_selection import SelectKBest,mutual_info_regression
-from sklearn import linear_model
-import math
-from sklearn.metrics import f1_score,precision_score,recall_score,accuracy_score,mean_squared_error,confusion_matrix
 
-tourist=pandas.read_csv("csvfile.csv")
+model = pickle.load(open('MachineLearningModel.pkl', 'rb'))
 
-mapping = {'Yes': 1, 'No': 0 }
+#perform tests on the loaded model
 
-columns = tourist.columns.tolist()
+inputInstance = []   #The input given by the user will be stored here
 
-target="Rating"
-columns=[c for c in columns if c not in["Rating","City"]]
+inputInstance = np.array(input).reshape(1,-1)    #Reshaping the instance to be accepted by the model
 
-for column in columns:
-    tourist=tourist.replace({column: mapping})
-    
-
-train= tourist.sample(frac=0.8,random_state=1)
-test=  tourist.loc[~tourist.index.isin(train.index)]
-
-trainAttributes=train[columns].astype('float64')
-trainTarget=train[target].astype('float64')
-testAttributes=test[columns].astype('float64')
-testTarget=test[target].astype('float64')
-testTarget=list(testTarget)
-
-regr = DecisionTreeRegressor(max_depth=2)
-#regr=linear_model.LinearRegression()
-
-regr.fit(trainAttributes,trainTarget)
-
-count=0;
-
-predictedList=[]
-actualList=[]
-
-for x in range(0,len(testTarget)):
-    h=testAttributes.iloc[x]
-    h=np.array(h).reshape(1,-1)
-    p= int(round(float(regr.predict(h))))
-    a= int(round(float(testTarget[x])))
-    predictedList.append(p)
-    actualList.append(a)
-    #print(regr.predict(h)," ",testTarget[x])
-
-print("Accuracy Score: ",accuracy_score(actualList,predictedList))
-print("Mean Squared Error: ",mean_squared_error(actualList,predictedList))
-print("F1_score: ",f1_score(actualList,predictedList,average="macro"))
-print("Precision Score: ",precision_score(actualList,predictedList,average="macro"))
-print("Recall Score: ",recall_score(actualList,predictedList,average="macro"))
-print("Confusion Matrix: ")
-print(confusion_matrix(actualList,predictedList))
-#print(regr.score(newtestX,testTarget))
+predictedOutputRating = int(round(float(model.predict(inputInstance)))) #Return the Output rating to the user in the app
