b8c3d643df078e6e642bd7865ed1d7aa30c5beb7
udaycoder <udayanbaidya@gmail.com>
2018-03-28 00:31:04 +0530

    Feature extraction trained on entire wiki corpus

---
+++ feature_extraction.py
@@ -0,0 +1,126 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Mar 22 14:17:46 2018
+
+@author: udaycoder
+"""
+# =============================================================================
+# import re
+# import nltk
+# from sklearn.feature_extraction.text import CountVectorizer
+#from sklearn.feature_extraction.text import TfidfVectorizer
+# =============================================================================
+import os
+import pickle
+import numpy as np
+import nltk
+# =============================================================================
+# from nltk import word_tokenize          
+# from nltk.stem import PorterStemmer
+# =============================================================================
+
+# =============================================================================
+# file = open("corpus.txt","r")
+# 
+# document = file
+# 
+# document = re.sub('[^A-Za-z .-]+', ' ', str(document))
+# document = document.replace('-', '')
+# document = document.replace('…', '')
+# document = document.replace('Mr.', 'Mr').replace('Mrs.', 'Mrs')
+# 
+# 
+# with open("modified_corpus.txt", "w") as f1:
+#     f1.write(document)
+# 
+# file.close()
+# =============================================================================
+
+
+root_dir = r"raw_en"
+
+walker = os.walk(root_dir)
+
+list_of_files = []
+
+words = set(nltk.corpus.words.words())
+
+for root,dirs,files in walker:
+    for f in files:
+        list_of_files.append(str(os.path.join(root,f)))
+# =============================================================================
+#         data = open(str(os.path.join(root,f)),"r",encoding='windows-1252')
+#         content = data.read()
+#         content = " ".join(w for w in nltk.wordpunct_tokenize(content) if w.lower() in words)
+#         data.close()
+#         data = open(str(os.path.join(root,f)),"w",encoding='utf-8')
+#         data.write(content)
+#         data.close()
+# =============================================================================
+        
+    
+
+# =============================================================================
+# count_vect = CountVectorizer(input='filename', encoding='windows-1252')
+# =============================================================================
+        
+# =============================================================================
+# count_vect = pickle.load(open('cvectorizer.pkl', 'rb'))
+# =============================================================================
+
+# =============================================================================
+# freq_term_matrix = count_vect.fit_transform(list_of_files)
+# =============================================================================
+
+# =============================================================================
+# filename = 'cvectorizer.pkl'
+# pickle.dump(count_vect , open(filename, 'wb'))
+# =============================================================================
+
+# =============================================================================
+# ps = PorterStemmer()
+# 
+# class stemTokenizer(object):
+#     def __call__(self, articles):
+#         return (ps.stem(t) for t in word_tokenize(articles))
+# 
+# 
+# tfidf = TfidfVectorizer(norm="l2",input='filename',stop_words='english',tokenizer=stemTokenizer(), lowercase = True)
+# =============================================================================
+        
+# =============================================================================
+# tfidf = TfidfVectorizer(norm="l2",input='filename',stop_words='english', lowercase = True)
+# tfidf.fit(list_of_files)
+# 
+# filename = 'tfidf.pkl'
+# pickle.dump(tfidf , open(filename, 'wb'))
+# =============================================================================
+
+tfidf = pickle.load(open('tfidf.pkl', 'rb'))
+
+
+# =============================================================================
+# doc_freq_term = count_vect.transform(document_file_name)
+# =============================================================================
+
+document_file_name = ['corpus.txt']
+
+doc_tfidf_matrix = tfidf.transform(document_file_name)
+
+scores = zip(tfidf.get_feature_names(),
+                 np.asarray(doc_tfidf_matrix.sum(axis=0)).ravel())
+sorted_scores = sorted(scores, key=lambda x: x[1], reverse=True)
+
+top_n = 50
+
+words = set(nltk.corpus.words.words())
+
+count = 0
+
+for item in sorted_scores:
+    if item[0] in words:
+        print("{0:50} Score: {1}".format(item[0], item[1]))
+        count+=1
+    if count == top_n:
+        break
