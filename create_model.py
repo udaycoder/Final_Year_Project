11df82e317225a6b7d5415f94474fd5d36145a8f
udaycoder <udayanbaidya@gmail.com>
2018-04-11 17:17:42 +0530

    added total rating counts

--- create_model.py
+++ create_model.py
@@ -24,6 +24,37 @@ trainAttributes=train[columns].astype('float64').apply(np.floor)
 trainTarget=train[target].astype('float64').apply(np.floor)
 testAttributes=test[columns].astype('float64').apply(np.floor)
 testTarget=test[target].astype('float64').apply(np.floor)
+
+count1=0
+count2=0
+count3=0
+count4=0
+count5=0
+
+for x in trainTarget:
+    if x==1:
+       count1+=1
+    if x==2:
+        count2+=1
+    if x==3:
+        count3+=1
+    if x==4:
+        count4+=1
+    if x==5:
+        count5+=1
+
+for x in testTarget:
+    if x==1:
+       count1+=1
+    if x==2:
+        count2+=1
+    if x==3:
+        count3+=1
+    if x==4:
+        count4+=1
+    if x==5:
+        count5+=1
+
 testTarget=list(testTarget)
 
 regr = DecisionTreeRegressor(max_depth=2)
@@ -76,3 +107,9 @@ print("Confusion Matrix: ")
 for x in Matrix:
      print(x)
 # =============================================================================
+
+print("Number of 1",count1)
+print("Number of 2",count2)
+print("Number of 3",count3)
+print("Number of 4",count4)
+print("Number of 5",count5)
