91e79d4e82b125762a50a2b65928891d8963f8db
udaycoder <udayanbaidya@gmail.com>
2018-04-11 10:32:29 +0530

    Added accuracies for floored values

--- create_model.py
+++ create_model.py
@@ -8,6 +8,7 @@ from sklearn.metrics import f1_score,precision_score,recall_score,accuracy_score
 from sklearn import svm
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.naive_bayes import GaussianNB
+import math
 
 tourist=pandas.read_csv("csvfile.csv")
 
@@ -19,10 +20,10 @@ test=  tourist.loc[~tourist.index.isin(train.index)]
 target="Rating"
 columns=[c for c in columns if c not in["Rating","City"]]  
 
-trainAttributes=train[columns].astype('float64').round()
-trainTarget=train[target].astype('float64').round()
-testAttributes=test[columns].astype('float64').round()
-testTarget=test[target].astype('float64').round()
+trainAttributes=train[columns].astype('float64')
+trainTarget=train[target].astype('float64')
+testAttributes=test[columns].astype('float64')
+testTarget=test[target].astype('float64')
 testTarget=list(testTarget)
 
 regr = DecisionTreeRegressor(max_depth=2)
@@ -31,7 +32,7 @@ svm_clf = svm.SVC( kernel='rbf')
 rndf_clf = RandomForestClassifier(max_depth=2, random_state=0)
 gnb = GaussianNB()
 
-svm_clf.fit(trainAttributes,trainTarget)
+lregr.fit(trainAttributes,trainTarget)
 
 # =============================================================================
 # filename = 'MachineLearningModel.pkl'
@@ -52,26 +53,26 @@ print("Actual\tPredicted")
 for x in range(0,len(testTarget)):
     h=testAttributes.iloc[x]
     h=np.array(h).reshape(1,-1)
-    p= int(round(float(svm_clf.predict(h))))
-    a= int(round(float(testTarget[x])))
+    p= int(math.floor(float(lregr.predict(h))))
+    a= int(math.floor(float(testTarget[x])))
 # =============================================================================
 #     p= float(rndf.predict(h))
 #     a= float(testTarget[x])
 # =============================================================================
     print(a,"\t",p)
-#    Matrix[a-1][p-1] += 1 
+    Matrix[a-1][p-1] += 1 
     predictedList.append(p)
     actualList.append(a)
     #print(regr.predict(h)," ",testTarget[x])
 
-#print("R2 Score ", r2_score(actualList,predictedList))
+print("R2 Score ", r2_score(actualList,predictedList))
 print("Accuracy Score: ",accuracy_score(actualList,predictedList))
 print("Mean Squared Error: ",mean_squared_error(actualList,predictedList))
 print("F1_score: ",f1_score(actualList,predictedList,average="macro"))
 print("Precision Score: ",precision_score(actualList,predictedList,average="macro"))
 print("Recall Score: ",recall_score(actualList,predictedList,average="macro"))
 # =============================================================================
-# print("Confusion Matrix: ")
-# for x in Matrix:
-#     print(x)
+print("Confusion Matrix: ")
+for x in Matrix:
+     print(x)
 # =============================================================================
