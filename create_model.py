d0d6dc84bbb2998a7c963cb08ce9b4864ffbedeb
udaycoder <udayanbaidya@gmail.com>
2018-04-11 10:10:49 +0530

    Added accuracies for floor values

--- create_model.py
+++ create_model.py
@@ -7,6 +7,7 @@ import pickle
 from sklearn.metrics import f1_score,precision_score,recall_score,accuracy_score,mean_squared_error,confusion_matrix,r2_score
 from sklearn import svm
 from sklearn.ensemble import RandomForestClassifier
+import math
 
 tourist=pandas.read_csv("csvfile.csv")
 
@@ -19,10 +20,10 @@ columns=[c for c in columns if c not in["Rating","City"]]
 train= tourist.sample(frac=0.8,random_state=1)
 test=  tourist.loc[~tourist.index.isin(train.index)]
 
-trainAttributes=train[columns].astype('float64')
-trainTarget=train[target].astype('float64')
-testAttributes=test[columns].astype('float64')
-testTarget=test[target].astype('float64')
+trainAttributes=train[columns].astype('float64').apply(np.floor)
+trainTarget=train[target].astype('float64').apply(np.floor)
+testAttributes=test[columns].astype('float64').apply(np.floor)
+testTarget=test[target].astype('float64').apply(np.floor)
 testTarget=list(testTarget)
 
 regr = DecisionTreeRegressor(max_depth=2)
@@ -30,7 +31,7 @@ lregr=linear_model.LinearRegression()
 svm_clf = svm.SVC( kernel='rbf')
 rndf_clf = RandomForestClassifier(max_depth=2, random_state=0)
 
-lregr.fit(trainAttributes,trainTarget)
+svm_clf.fit(trainAttributes,trainTarget)
 
 filename = 'MachineLearningModel.pkl'
 pickle.dump(regr, open(filename, 'wb'))
@@ -49,27 +50,27 @@ print("Actual\tPredicted")
 for x in range(0,len(testTarget)):
     h=testAttributes.iloc[x]
     h=np.array(h).reshape(1,-1)
-    p= int(round(float(lregr.predict(h))))
-    a= int(round(float(testTarget[x])))
+    p= int(math.floor(float(svm_clf.predict(h))))
+    a= int(math.floor(float(testTarget[x])))
 # =============================================================================
 #     p= float(regr.predict(h))
 #     a= float(testTarget[x])
 # =============================================================================
     print(a,"\t",p)
-    #Matrix[a-1][p-1] += 1 
+    Matrix[a-1][p-1] += 1 
     predictedList.append(p)
     actualList.append(a)
     #print(regr.predict(h)," ",testTarget[x])
 
-print("r2 score: ",r2_score(actualList,predictedList))
+#print("r2 score: ",r2_score(actualList,predictedList))
 print("Accuracy Score: ",accuracy_score(actualList,predictedList))
 print("Mean Squared Error: ",mean_squared_error(actualList,predictedList))
 print("F1_score: ",f1_score(actualList,predictedList,average="macro"))
 print("Precision Score: ",precision_score(actualList,predictedList,average="macro"))
 print("Recall Score: ",recall_score(actualList,predictedList,average="macro"))
-# =============================================================================
-# print("Confusion Matrix: ")
-# for x in Matrix:
-#     print(x)
-# =============================================================================
+
+print("Confusion Matrix: ")
+for x in Matrix:
+     print(x)
+
 #print(regr.score(newtestX,testTarget))
